b0ec1879131fa0134f16bd016793b429a6000fa3
Trevor Rudolph <cool.test.cool@gmail.com>
2025-01-09 02:28:41 -0500

    fix ipl detection

--- ppc/apploader/hacks.c
+++ ppc/apploader/hacks.c
@@ -3,6 +3,7 @@
 
 #include "types.h"
 #include "libc.h"
+#include "ipl.h"
 
 #include "crc32.h"
 #include "usbgecko.h"
@@ -15,52 +16,6 @@ extern void run(register void* entry_point);
 extern void run_interrupt(register void *entry_point);
 extern void load_dol();
 extern void boot_dol();
-
-enum ipl_revision {
-	IPL_UNKNOWN,
-	IPL_NTSC_10_001,
-	IPL_NTSC_10_002,
-	IPL_DEV_10,
-	IPL_NTSC_11_001,
-	IPL_PAL_10_001,
-	IPL_PAL_10_002,
-	IPL_MPAL_11,
-	IPL_TDEV_11,
-	IPL_NTSC_12_001,
-	IPL_NTSC_12_101,
-	IPL_PAL_12_101
-};
-
-static enum ipl_revision get_ipl_revision(void)
-{
-	register uint32_t sdata2 asm ("r2");
-	register uint32_t sdata asm ("r13");
-
-	if (sdata2 == 0x81465cc0 && sdata == 0x81465320)
-		return IPL_NTSC_10_001;
-	if (sdata2 == 0x81468fc0 && sdata == 0x814685c0)
-		return IPL_NTSC_10_002;
-	if (sdata2 == 0x814695e0 && sdata == 0x81468bc0)
-		return IPL_DEV_10;
-	if (sdata2 == 0x81489c80 && sdata == 0x81489120)
-		return IPL_NTSC_11_001;
-	if (sdata2 == 0x814b5b20 && sdata == 0x814b4fc0)
-		return IPL_PAL_10_001;
-	if (sdata2 == 0x814b4fc0 && sdata == 0x814b4400)
-		return IPL_PAL_10_002;
-	if (sdata2 == 0x81484940 && sdata == 0x81483de0)
-		return IPL_MPAL_11;
-	if (sdata2 == 0x8148fbe0 && sdata == 0x8148ef80)
-		return IPL_TDEV_11;
-	if (sdata2 == 0x8148a660 && sdata == 0x8148b1c0)
-		return IPL_NTSC_12_001;
-	if (sdata2 == 0x8148aae0 && sdata == 0x8148b640)
-		return IPL_NTSC_12_101;
-	if (sdata2 == 0x814b66e0 && sdata == 0x814b7280)
-		return IPL_PAL_12_101;
-
-	return IPL_UNKNOWN;
-}
 
 #define CONST(...) __VA_ARGS__
 
@@ -196,8 +151,7 @@ void fix_ipl_state(void) {
 }
 
 void inception() {
-	u16 *signal_val = (void*)0x81300000;
-	if (*signal_val == 0x4800) {
+	if (get_ipl_revision() != IPL_UNKNOWN) {
 		gprintf("Early code exec\n");
 		gprintf("INCEPTION\n");
 		run_interrupt((void*)new_routine);
